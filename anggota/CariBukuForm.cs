364c1e4c8a6f171b04f0e39fef924d7001f43540
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-12-03 13:47:43 +0700

    better query on anggota cari buku

--- anggota/CariBukuForm.cs
+++ anggota/CariBukuForm.cs
@@ -30,7 +30,7 @@ namespace studikasus_smk_nasional.anggota
         private void showAllBook()
         {
             sql_utilities.con.Open();
-            string query = "Select * from buku";
+            string query = "Select kode_buku, judul, penulis, penerbit, nama_kat, lantai, rak, label, stok from buku join kategorri on kategorri.id_kat = buku.id_kat join lokasi on lokasi.kode_lokasi = buku.kode_lokasi";
             sql_utilities.cmd = new SqlCommand(query, sql_utilities.con);
             
 
@@ -47,7 +47,7 @@ namespace studikasus_smk_nasional.anggota
         {
             if (!string.IsNullOrEmpty(edtSearchBook.Text)){
                 sql_utilities.con.Open();
-                string query = "Select * from buku where judul like '%" + edtSearchBook.Text + "%'";
+                string query = "Select kode_buku, judul, penulis, penerbit, nama_kat, lantai, rak, label, stok from buku join kategorri on kategorri.id_kat = buku.id_kat join lokasi on lokasi.kode_lokasi = buku.kode_lokasi where judul like '%" + edtSearchBook.Text + "%'";
                 sql_utilities.cmd = new SqlCommand(query, sql_utilities.con);
 
                 dataTable.Clear(); // clear the data table
